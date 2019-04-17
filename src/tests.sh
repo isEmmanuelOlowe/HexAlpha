a8c4e0413f0f5eca7962aa373e4d44d9ae3ad0d6
Emmanuel Olowe <eo32@st-andrews.ac.uk>
2019-04-17 16:37:04 +0100

    testing changes

--- src/tests.sh
+++ src/tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ./build.sh
-javac -cp junit-4.12.jar:. Tests/*.java
-java -cp junit-4.12.jar:. Tests.GameTests
+javac -cp "tests/junit-4.12.jar:tests/hamcrest-core-1.3.jar:." tests/*.java
+java -cp "tests/junit-4.12.jar:tests/hamcrest-core-1.3.jar:." org.junit.runner.JUnitCore tests.GameTests
