1dc6ccff731122d21147ddfc1b2d2583fee3ccb5
eo32 <eo32@pc2-117-l.cs.st-andrews.ac.uk>
2019-04-17 16:17:23 +0100

    captialisation fix

--- src/build.sh
+++ src/build.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
 echo "Builing Project: hexalpha"
 
-function build() {
-  echo "Building: $1"
-  if [ ! -f $1*.class ]; then
-    rm $1*.class
-  fi
-  javac $1*.java
-}
-
-
-build hexalpha/
-build hexalpha/controller/
-build hexalpha/controller/splash/
-build hexalpha/controller/game/
-build hexalpha/controller/winner/
-build hexalpha/engine/
-
+cd hexalpha
+shopt -s globstar
+javac **/*.java
+cd ..
 jar cvfm HexAlpha.jar manifest.txt hexalpha
